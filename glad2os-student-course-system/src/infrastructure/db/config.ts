647a75ea4fceb1f17cb0fcc128184d0288d6cff7
glad2os <lite.console4bit@gmail.com>
2024-02-06 23:52:54 -0500

    _

--- glad2os-student-course-system/src/infrastructure/db/config.ts
+++ glad2os-student-course-system/src/infrastructure/db/config.ts
@@ -5,6 +5,7 @@ const mongoPort = process.env.MONGO_PORT || 27017;
 const mongoDatabase = process.env.MONGO_DATABASE || 'your_database_name';
 const mongoURL = `mongodb://${mongoHost}:${mongoPort}/${mongoDatabase}`;
 
+console.log(`${process.env.MONGO_HOST}:${process.env.MONGO_PORT}`);
 // Connect to MongoDB
 mongoose.connect(mongoURL)
   .then(() => {
