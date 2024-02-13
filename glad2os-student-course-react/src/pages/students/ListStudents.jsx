23140dc30f78b64268d289ba2f1af85f46ed0d0a
glad2os <lite.console4bit@gmail.com>
2024-02-12 21:56:36 -0500

    _

--- glad2os-student-course-react/src/pages/students/ListStudents.jsx
+++ glad2os-student-course-react/src/pages/students/ListStudents.jsx
@@ -1,7 +1,37 @@
-function ListStudents() {
-    return <div>
-        ListStudents
-    </div>
-}
+import React, { useEffect, useState } from 'react';
+import api from '../../configs/api.js';
+
+const ListStudents = () => {
+    const [students, setStudents] = useState([]);
+
+    useEffect(() => {
+        const fetchStudents = async () => {
+            try {
+                const response = await api.get('http://localhost:3000/students');
+                setStudents(response.data);
+            } catch (error) {
+                console.error("Error fetching data: ", error);
+                // Handle error response (e.g., display an error message)
+            }
+        };
+
+        fetchStudents().then(r => {
+            console.log(r)
+        });
+    }, []);
+
+    return (
+        <div>
+            <h1>Students</h1>
+            <ul>
+                {students.map(student => (
+                    <li key={student._id}>
+                        {student.firstName} {student.lastName} - {student.program}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+};
 
 export default ListStudents;
--- glad2os-student-course-system/src/index.ts
+++ glad2os-student-course-system/src/index.ts
@@ -2,16 +2,21 @@ import courseRoutes from '@routes/courseRoutes';
 import { studentRoutes } from '@routes/studentRoutes';
 import express, { Express } from 'express';
 
+const cors = require('cors')
+
+
 
 const app: Express = express();
 const port = process.env.PORT || 25600;
 
-app.use(express.json()); // for parsing application/json
+app.use(express.json());
+app.use(cors({
+  origin: 'http://localhost:5173'
+}));
 
 // Setup routes
 app.use('/students', studentRoutes);
 app.use('/courses', courseRoutes);
-
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
